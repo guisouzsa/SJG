58bb0a0f5adb36749d6d9579f1ed338da143427c
gui <168765125+guisouzsa@users.noreply.github.com>
2025-12-02 16:27:06 -0300

    fix: corrigir máscara de CPF e bloquear caracteres não numéricos
    Atualização do campo CPF para aceitar somente números e aplicar a máscara 000.000.000-00 usando iMask. Adicionado filtro 'prepare' para impedir letras, símbolos e caracteres inválidos.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,38 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import IMask from 'imask';
+
+const cpfElement = document.getElementById('cpf_Cnpj');
+
+if (cpfElement) {
+    IMask(cpfElement, {
+        mask: '000.000.000-00',
+        prepare: function (str) {
+            return str.replace(/\D/g, ''); // remove tudo que NÃO é número
+        }
+    });
+}
+
+
+const telefoneElement = document.getElementById('telefone');
+if (telefoneElement) {
+    IMask(telefoneElement, {
+        mask: [
+            {
+                mask: '(00) 0000-0000'
+            },
+            {
+                mask: '(00) 00000-0000'
+            }
+        ]
+    });
+}
+
+const emailElement = document.getElementById('email');
+if (emailElement) {
+    IMask(emailElement, {
+        mask: /^\S*@?\S*$/
+    });
+}
