1d2263b4277724ffac5af63351bd22f3e459713d
guisouzsa <guilherme.souza103@aluno.ce.gov.br>
2025-12-03 00:22:00 -0300

    feat(processos): adicionar CreateProcessoRequest
    - Define regras de validação para criação de processos jurídicos.
    - Garante unicidade do número do processo e valida dados essenciais.

---
+++ app/Http/Requests/processos/CreateProcessoRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\processos;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateProcessoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'numero_processo' => 'required|string|unique:processos,numero_processo',
+            'tipo'            => 'required|string|max:100',
+            'status'          => 'required|string|max:100',
+            'descricao'       => 'nullable|string',
+        ];
+    }
+}
