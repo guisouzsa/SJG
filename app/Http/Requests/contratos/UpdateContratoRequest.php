fbffb8ffa52d8e1187d5b1f8dccb765cde32c075
guisouzsa <guilherme.souza103@aluno.ce.gov.br>
2025-12-03 00:24:51 -0300

    feat(contratos): adicionar UpdateContratoRequest
    - Valida atualização de contratos jurídicos.
    - Mantém integridade de datas, valores e vinculação com clientes.

---
+++ app/Http/Requests/contratos/UpdateContratoRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\contratos;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateContratoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $id = $this->route('contrato') ?? $this->route('id');
+
+        return [
+            'titulo'         => 'required|string|max:255',
+            'tipo'           => 'required|string|max:100',
+            'cliente_id'     => 'nullable|exists:clientes,id',
+            'data_assinatura'=> 'required|date',
+            'data_vencimento'=> 'required|date|after_or_equal:data_assinatura',
+            'valor'          => 'required|numeric|min:0',
+            'descricao'      => 'nullable|string',
+        ];
+    }
+}
